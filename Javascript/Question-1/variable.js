0476e0c81f45e95c9007e36c1db11a3d5603ac45
PriyanshiNegi <priyanshiii1103@gmail.com>
2023-01-14 23:51:55 +0530

    JS Q-1

---
+++ Javascript/Question-1/variable.js
@@ -0,0 +1,22 @@
+// String
+
+const FristName="Priyanshi";
+console.log("I am a String", FristName);
+
+// Boolean
+
+const flag=true;
+console.log("I am a boolean value", flag);
+
+// Number
+
+const num= 42;
+console.log("I am a number", num);
+
+//Null
+
+const value=null;
+console.log("I have value set as", value);
+
+//undefined
+
